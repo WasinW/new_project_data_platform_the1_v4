4af966295daa7a32dd7cc9747202607994e769bc
wasin <wawasin@central.co.th>
2025-11-30 19:49:46 +0700

    fix

--- path_compare/dag_ms_member_short_term_init.py
+++ path_compare/dag_ms_member_short_term_init.py
@@ -57,16 +57,24 @@ def get_secret_value(secret_id, project_id):
     name = f"projects/{project_id}/secrets/{secret_id}/versions/latest"
     try:
         response = client.access_secret_version(request={"name": name})
-        return json.loads(response.payload.data.decode("UTF-8"))
+        # for stg/prod
+        # return json.loads(response.payload.data.decode("UTF-8"))
+        # for dev (single value)
+        return response.payload.data.decode("UTF-8")
+
     except Exception as e:
         logger.error(f"Failed to get secret {secret_id}: {e}")
         raise
 
 def get_aws_credentials(**context):
     """Get AWS credentials and push to XCom"""
-    access_key = get_secret_value('insight-data-pipeline', PROJECT_ID)['aws-access-key']
-    secret_key = get_secret_value('insight-data-pipeline', PROJECT_ID)['aws-secret-key']
-    
+    # fot stg/prod
+    # access_key = get_secret_value('insight-data-pipeline', PROJECT_ID)['aws-access-key']
+    # secret_key = get_secret_value('insight-data-pipeline', PROJECT_ID)['aws-secret-key']
+    # fot dev
+    access_key = get_secret_value('data-pipeline-aws-access-key', PROJECT_ID)
+    secret_key = get_secret_value('data-pipeline-aws-secret-key', PROJECT_ID)
+
     # Push to XCom for next tasks
     context['ti'].xcom_push(key='aws_access_key', value=access_key)
     context['ti'].xcom_push(key='aws_secret_key', value=secret_key)
@@ -147,32 +155,32 @@ monitor_mapping_transfer = BigQueryDataTransferServiceTransferRunSensor(
     dag=dag,
 )
 
-# Task 3: Trigger MS member transfer
-trigger_member_transfer = BigQueryDataTransferServiceStartTransferRunsOperator(
-    task_id="trigger_ms_member_transfer",
-    project_id=PROJECT_ID,
-    location=REGION,
-    transfer_config_id='{{ var.value.member_transfer_config_id }}',
-    requested_run_time={"seconds": int(time.time())},
-    gcp_conn_id=GCP_CONN_ID,
-    deferrable=True,
-    dag=dag,
-)
+# # Task 3: Trigger MS member transfer
+# trigger_member_transfer = BigQueryDataTransferServiceStartTransferRunsOperator(
+#     task_id="trigger_ms_member_transfer",
+#     project_id=PROJECT_ID,
+#     location=REGION,
+#     transfer_config_id='{{ var.value.member_transfer_config_id }}',
+#     requested_run_time={"seconds": int(time.time())},
+#     gcp_conn_id=GCP_CONN_ID,
+#     deferrable=True,
+#     dag=dag,
+# )
 
-# Task 4: Monitor member transfer completion
-monitor_member_transfer = BigQueryDataTransferServiceTransferRunSensor(
-    task_id="monitor_member_transfer",
-    transfer_config_id='{{ var.value.member_transfer_config_id }}',
-    run_id="{{ ti.xcom_pull(task_ids='trigger_ms_member_transfer', key='run_id') }}",
-    expected_statuses={"SUCCEEDED"},
-    project_id=PROJECT_ID,
-    location=REGION,
-    poke_interval=60,
-    timeout=600,
-    mode="poke",
-    gcp_conn_id=GCP_CONN_ID,
-    dag=dag,
-)
+# # Task 4: Monitor member transfer completion
+# monitor_member_transfer = BigQueryDataTransferServiceTransferRunSensor(
+#     task_id="monitor_member_transfer",
+#     transfer_config_id='{{ var.value.member_transfer_config_id }}',
+#     run_id="{{ ti.xcom_pull(task_ids='trigger_ms_member_transfer', key='run_id') }}",
+#     expected_statuses={"SUCCEEDED"},
+#     project_id=PROJECT_ID,
+#     location=REGION,
+#     poke_interval=60,
+#     timeout=600,
+#     mode="poke",
+#     gcp_conn_id=GCP_CONN_ID,
+#     dag=dag,
+# )
 
 # Add task to get credentials
 get_credentials = PythonOperator(
@@ -186,7 +194,8 @@ get_credentials = PythonOperator(
 dataflow_job = BeamRunPythonPipelineOperator(
     task_id='run_dataflow_pipeline',
     runner='DataflowRunner',
-    py_file='{{ var.value.bucket_composer }}/dataflow/scripts/ms_member_short_pipeline.py',
+    # py_file='{{ var.value.bucket_composer }}/dataflow/scripts/ms_member_short_pipeline.py',
+    py_file='{{ var.value.bucket_dataflow }}/jobs/ms_member_short_pipeline.py',
 
     # Dataflow pipeline options
     # ----------------------------
@@ -205,12 +214,12 @@ dataflow_job = BeamRunPythonPipelineOperator(
         
         # Worker configuration
         # 'worker_machine_type': 'n1-standard-2',
-        'worker_machine_type': 'n1-standard-8',
+        'worker_machine_type': 'n1-standard-4',
         'max_num_workers': 8,
-        'num_workers': 2,
+        'num_workers': 4,
         'disk_size_gb': 100,
-        'number_of_worker_harness_threads': 4,
-        'save_main_session': False,
+        'number_of_worker_harness_threads': 8,
+        'save_main_session': True,
         # ------------------------------------------------------------------------------------
         # Standard persistent disk (lowest cost but slowest)
         # 'worker_disk_type': 'compute.googleapis.com/projects//zones//diskTypes/pd-standard',
@@ -228,7 +237,6 @@ dataflow_job = BeamRunPythonPipelineOperator(
             #  shuffle_mode : +$0.048/GB shuffled (~1.6 bath/GB)
             # Reduced disk I/O on worker , Better Scale , Reduced issue disk space exhaustion
             ,'worker_heap_size_mb=15000' 
-            , 'no_use_multiple_sdk_containers'
             # 'min_cpu_platform=Intel Skylake'  # ← ใส่ตรงนี้ถ้าอยากใช้
             ],
         # Control log levels
@@ -245,7 +253,8 @@ dataflow_job = BeamRunPythonPipelineOperator(
         # Pipeline parameters
         'project_id': PROJECT_ID,
         'mode': 'batch',
-        'config_path': '{{ var.value.bucket_composer }}/config/ms_member_short_init.yaml',
+        # 'config_path': '{{ var.value.bucket_composer }}/config/ms_member_short_init.yaml',
+        'config_path': '{{ var.value.bucket_config }}/dags/composer/config/ms_member/batch/ms_member_short_init.yaml',
         
         # AWS S3 credentials
         's3_region_name': 'ap-southeast-1',
@@ -253,7 +262,8 @@ dataflow_job = BeamRunPythonPipelineOperator(
         's3_secret_access_key': "{{ ti.xcom_pull(task_ids='get_aws_credentials', key='aws_secret_key') }}",
 
         'labels': {
-            'environment': '{WORKSPACE_ENV}',
+            # 'environment': '{WORKSPACE_ENV}',
+            'environment': 'dev',  # หรือ dev/staging
             'pipeline': 'ms-personas-short-init',
             'team': 'data-team',
             'cost-center': 'data-engineering',
@@ -270,13 +280,13 @@ dataflow_job = BeamRunPythonPipelineOperator(
         'apache-beam[gcp]==2.69.0',
         'google-cloud-bigquery==3.25.0',
         'pyarrow==14.0.2',
-        'numpy<2',  # CRITICAL: pyarrow requires numpy 1.x
+        # 'numpy<2',  # CRITICAL: pyarrow requires numpy 1.x
         'pyyaml>=6.0',
-        's3fs==2024.6.1',
-        'fsspec==2024.6.1',
-        'aiobotocore==2.13.0',
-        'boto3==1.34.106',
-        'botocore==1.34.106',
+        # 's3fs==2024.6.1',
+        # 'fsspec==2024.6.1',
+        # 'aiobotocore==2.13.0',
+        # 'boto3==1.34.106',
+        # 'botocore==1.34.106',
         '/home/airflow/gcs/dags/packages/dataflow_common-1.0.0-py3-none-any.whl',
     ],
     py_system_site_packages=False,
@@ -317,11 +327,12 @@ dataflow_job = BeamRunPythonPipelineOperator(
 # ============================================
 # # Parallel transfers
 trigger_mapping_transfer >> monitor_mapping_transfer
-trigger_member_transfer >> monitor_member_transfer
+# trigger_member_transfer >> monitor_member_transfer
 
 # After both transfers complete -> pre-check -> dataflow -> wait
 # [monitor_mapping_transfer, monitor_member_transfer] >> pre_check >> dataflow_job >> wait_dataflow
 # [monitor_mapping_transfer, monitor_member_transfer] >> pre_check >> dataflow_job >> check_cost
 # [monitor_mapping_transfer, monitor_member_transfer] >> pre_check >> dataflow_job
-[monitor_mapping_transfer, monitor_member_transfer] >> pre_check >> get_credentials >> dataflow_job
+# [monitor_mapping_transfer, monitor_member_transfer] >> pre_check >> get_credentials >> dataflow_job
+[monitor_mapping_transfer] >> pre_check >> get_credentials >> dataflow_job
 # pre_check >> dataflow_job >> wait_dataflow
